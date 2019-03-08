dddddcb076956589a708d97ff72e5211b0d50280
guiaixiao <824541050@qq.com>
2019-03-08 15:04:36 +0800

    更改项目结构

--- flasky_blog.py
+++ flasky_blog.py
@@ -1,8 +1,7 @@
-from datetime import datetime
 from flask import Flask,render_template,url_for,flash,redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm,LoginForm
-
+from models import User,Post
 
 
 app = Flask(__name__)
@@ -14,26 +13,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #不加python shell导入�
 '''
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20),unique=True,nullable=False)
-    email = db.Column(db.String(120),unique=True,nullable=False)
-    image_file = db.Column(db.String(20),nullable=False,default='default.jpg')
-    password = db.Column(db.String(60),nullable=False)
-    posts = db.relationship('Post',backref='author',lazy=True)
 
-    def __repr__(self):
-        return f"User('{self.username}','{self.email}','{self.image_file}')"
-class Post(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    title = db.Column(db.String(100),nullable=False)
-    date_posted = db.Column(db.DateTime,nullable=False,default=datetime.utcnow)
-    #db.DateTime的T为大写
-    content = db.Column(db.Text,nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.title}','{self.date_posted}')"
 
 posts = [
     {
