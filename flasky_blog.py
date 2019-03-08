c7e36db32cb2a615ed6f546a119106eead2a73b4
guiaixiao <824541050@qq.com>
2019-03-08 13:29:58 +0800

    forms

--- flasky_blog.py
+++ flasky_blog.py
@@ -1,7 +1,7 @@
-from flask import Flask,render_template,url_for
-
+from flask import Flask,render_template,url_for,flash,redirect
+from forms import RegistrationForm,LoginForm
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 posts = [
     {
         'author': 'Corey Schafer',
@@ -26,6 +26,32 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+
+
+
+@app.route("/register",methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        #flash(f'账户已经为{form.username.data}创建!','success')
+        flash(f'{form.username.data} 您好，账户已经为您创建', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title='register',form=form)
+
+
+@app.route("/login",methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'guili618@gmail.com':
+            flash('您好，您已经成功登录', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('请检查您的用户名和密码是否正确，谢谢！')
+    return render_template('login.html',title='About',form=form)
+
+
 
 
 if __name__ == '__main__':
