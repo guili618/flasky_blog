76feecdf6fb08a6d31ba73a828da567c418a72aa
guiaixiao <824541050@qq.com>
2019-03-09 16:26:42 +0800

    20190309 User Account and Profile Picture

--- flasky_blog/forms.py
+++ flasky_blog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField,FileAllowed
+from flask_login import current_user
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
 from wtforms.validators import DataRequired,Length,Email,EqualTo,ValidationError
 from flasky_blog.models import User
@@ -30,3 +32,24 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('用户名',
+                            validators=[DataRequired(),Length(min=2,max=20)])
+    email = StringField('邮箱',
+                            validators=[DataRequired(),Email()])
+    picture = FileField('修改头像',validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('更新个人信息')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('该用户名已经被使用，请输入不同的用户名 ！')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('该email已经被使用，请输入不同的email ！')
