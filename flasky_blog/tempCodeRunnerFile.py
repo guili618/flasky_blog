b27cf0204c88bc097a5ca7816f6e3739a703c8c3
guiaixiao <824541050@qq.com>
2019-03-10 15:19:40 +0800

    Pagination

--- flasky_blog/tempCodeRunnerFile.py
+++ flasky_blog/tempCodeRunnerFile.py
@@ -1,62 +1,9 @@
-from flask import Flask,render_template,url_for,flash,redirect
+import os
+import secrets
+from PIL import Image
+from flask import Flask,render_template,url_for,flash,redirect,request,abort
 from flasky_blog import  app,db, bcrypt
 from flasky_blog.models import User,Post
-from flasky_blog.forms import RegistrationForm,LoginForm
-
-
-
-
-posts = [
-    {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
-]
-
-
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html',posts=posts)
-
-@app.route("/about")
-def about():
-    return render_template('about.html',title='About')
-
-
-
-
-@app.route("/register",methods=['GET','POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        #flash(f'账户已经为{form.username.data}创建!','success')
-        hashed_password = bcrypt.generate_password_hash(form.password.data).\
-                          decode('utf-8')
-        user = User(username=form.username.data,email=form.email.data,\
-               password = hashed_password  )
-        db.session.add(user)
-        db.session.commit()
-        flash(f'{form.username.data} 您好，账户已经为您创建', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html',title='register',form=form)
-
-
-@app.route("/login",methods=['GET','POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'guili618@gmail.com':
-            flash('您好，您已经成功登录', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('请检查您的用户名和密码是否正确，谢谢！')
-    return render_template('login.html',title='About',form=form)
+from flasky_blog.forms import RegistrationForm,LoginForm,UpdateAccountForm,\
+                              PostForm
+from flask_login import login_user,current_user,logout_user,login_required
