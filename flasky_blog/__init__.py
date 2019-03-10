e5382465b4d0a39e97e1e17ace5bd13393918e79
guiaixiao <824541050@qq.com>
2019-03-10 17:47:51 +0800

    add debug tool

--- flasky_blog/__init__.py
+++ flasky_blog/__init__.py
@@ -3,9 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 # from forms import RegistrationForm,LoginForm
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
+
+app.debug = False
+
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #不加python shell导入会报错
@@ -14,6 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #不加python shell导入�
 '''
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+toolbar = DebugToolbarExtension(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
